4d12b29bc5d9d53ff90896c00e84bcc65daa40db
vonMajkel <mpjurgo@gmail.com>
2018-09-28 16:46:00 +0200

    Add user features tests with capybara

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,15 @@ class User < ApplicationRecord
   ## The :root_admin can access any page regardless of access settings. Use with caution!   ##
   ## The multiple option can be set to true if you need users to have multiple roles.       ##
   petergate(roles: [:admin, :editor], multiple: false)                                      ##
-  ############################################################################################ 
- 
+  ############################################################################################
+
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :firstname, :lastname, length: { maximum: 30 }
 
   def name
    if !self.firstname.nil? || !self.lastname.nil?
---
+++ spec/features/users_spec.rb
@@ -0,0 +1,97 @@
+require 'rails_helper'
+
+RSpec.feature "Users", type: :feature do
+  context 'create new user' do
+    before(:each) do
+      visit new_user_registration_path
+      within('form') do
+        fill_in 'user_email', with: 'sienkiewicz@example.com'
+        fill_in 'user_password', with: '123456'
+      end
+    end
+
+    scenario 'should be successful' do
+      within('form') do
+        fill_in 'user_password_confirmation', with: '123456'
+      end
+      click_button 'Sign up'
+      expect(page).to have_content('Welcome! You have signed up successfully.')
+    end
+
+    scenario 'should fail' do
+      within('form') do
+        fill_in 'user_password_confirmation', with: '123457'
+      end
+      click_button 'Sign up'
+      expect(page).to have_content('Password confirmation doesn\'t match Password')
+    end
+  end
+
+  context 'log user in' do
+    let!(:user) { User.create(email: 'sienkiewicz@example.com', password: '123456', password_confirmation: '123456') }
+    before(:each) do
+      visit new_user_session_path
+    end
+
+    scenario 'should be successful' do
+      within('form') do
+        fill_in 'user_email', with: 'sienkiewicz@example.com'
+        fill_in 'user_password', with: '123456'
+      end
+      click_button 'Log in'
+      expect(page).to have_content 'Signed in successfully.'
+    end
+
+    scenario 'should fail' do
+      within('form') do
+        fill_in 'user_email', with: 'sienkiewicz@example.com'
+        fill_in 'user_password', with: '123457'
+      end
+      click_button 'Log in'
+      expect(page).to have_content 'Invalid Email or password.'
+    end
+  end
+
+  context 'log user out' do
+    let!(:user) { User.create(email: 'sienkiewicz@example.com', password: '123456', password_confirmation: '123456') }
+    before(:each) do
+      login_as(user, scope: :user)
+    end
+
+    scenario 'should be successful' do
+      visit root_path
+      click_link('Logout')
+      expect(page).to have_content 'Signed out successfully.'
+    end
+  end
+
+  context 'update user' do
+    let!(:user) { User.create(email: 'sienkiewicz@example.com', password: '123456', password_confirmation: '123456') }
+    before(:each) do
+      login_as(user, scope: :user)
+      visit edit_user_registration_path(user)
+    end
+
+    scenario 'should be successful' do
+      within('form') do
+        fill_in 'user_firstname', with: 'Henryk'
+        fill_in 'user_lastname', with: 'Sienkiewicz'
+        fill_in 'user_current_password', with: user.password
+      end
+      click_button 'Update'
+      expect(page).to have_content 'Your account has been updated successfully.'
+      expect(page).to have_content 'Signed in as Henryk Sienkiewicz'
+    end
+
+    scenario 'should fail' do
+      within('form') do
+        fill_in 'user_firstname', with: 'a'*31
+        fill_in 'user_lastname', with: 'a'*31
+      end
+      click_button 'Update'
+      expect(page).to have_content 'Current password can\'t be blank'
+      expect(page).to have_content 'Firstname is too long'
+      expect(page).to have_content 'Lastname is too long'
+    end
+  end
+end
