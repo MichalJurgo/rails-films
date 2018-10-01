471b70a4222a0321f1b468e5d038cd3425f87d66
vonMajkel <mpjurgo@gmail.com>
2018-10-01 15:35:28 +0200

    Add person show page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   resources :friendships, only: [:create, :update, :destroy]
-  resources :films
+  resources :films, only: [:index, :show]
+  resources :people, only: [:show]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -24,21 +24,21 @@ Genre.create!(name: "Musical")
 
 Film.create!(title: "Ogniem i Mieczem",
               short_description: "Adaptacja powieści Sienkiewicza, pierwsza część trylogii.",
-              description: Faker::Lorem.paragraphs(4).join,
+              description: Faker::Lorem.paragraphs(10).join,
               production: ["Polska"],
               release: Date.parse("1999-02-08"),
               duration: 175)
 
 Film.create!(title: "Django",
               short_description: "O łowcy nagród i czarnoskórym niewolniku.",
-              description: Faker::Lorem.paragraphs(4).join,
+              description: Faker::Lorem.paragraphs(10).join,
               production: ["USA"],
               release: Date.parse("2012-12-11"),
               duration: 165)
 
 Film.create!(title: "Infiltracja",
               short_description: "Tajny policjant i informator mafii.",
-              description: Faker::Lorem.paragraphs(4).join,
+              description: Faker::Lorem.paragraphs(10).join,
               production: ["Hongkong", "USA"],
               release: Date.parse("2006-10-06"),
               duration: 152)
@@ -46,7 +46,7 @@ Film.create!(title: "Infiltracja",
 Film.create!(title: "Król rozrywki",
               short_description: "Historia amerykańskiego artysy, P.T. Barnuma,
               założyciela cyrku.",
-              description: Faker::Lorem.paragraphs(4).join,
+              description: Faker::Lorem.paragraphs(10).join,
               production: ["USA"],
               release: Date.parse("2017-12-20"),
               duration: 105)
@@ -54,7 +54,7 @@ Film.create!(title: "Król rozrywki",
 Film.create!(title: "Młoda Wiktoria",
               short_description: "Wiktoria zostaje królową Zjednoczonego Królestwa
               Wielkiej Brytanii.",
-              description: Faker::Lorem.paragraphs(4).join,
+              description: Faker::Lorem.paragraphs(10).join,
               production: ["USA", "Wielka Brytania"],
               release: Date.parse("2009-03-06"),
               duration: 100)
@@ -72,7 +72,7 @@ Status.create!(name: "To see")
     lastname: Faker::Name.last_name,
     birthday: Faker::Date.between(80.years.ago, 25.years.ago),
     place_of_birth: "#{Faker::Address.city}, #{Faker::Address.country}",
-    biography: Faker::Lorem.paragraphs(3)
+    biography: Faker::Lorem.paragraphs(10).join
   )
 end
 
